c394f6365a6e67ddf04d995b29d69e019ea233bf
alisonkleffner <akleffner@huskers.unl.edu>
2023-08-18 14:40:43 -0500

    small edits to week 2 slides

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Date | Notes | Description | Additional Files | Homework Assigned
 ---- | ---- | --- | --- | ---
 8/15/2023 | [Welcome to Mth 361](Week 1/Intro-to-361.html) | Class Introductions | | Introduction Questionnaire (BlueLine) **due Thursday August 24 at 11:59 pm**
 8/17/2023 | [R Introduction](Week 1/IntroR.html) | Introduction to to R and R Markdown (What is it and why we would want to use it) | [R File](Week 1/IntroR.R)<br/>[R Markdown File](Week 1/R-Markdown_Intro.Rmd)<br/>[ICU Admissions Data](Week 1/data/ICUAdmissions.csv)| 
+8/22/2023 | [Intro to Data and Data Collection](Week 2/Week2-notes.html) | Describing variables and Data Collection Methods | | Homework 1 **due Tuesday August 29 at 11:59 pm**
+8/22/2023 | | Lab Day | | Lab 1 **due Friday August 25 at 11:59 pm**
--- Week 2/Week2-Notes.R
+++ Week 2/Week2-Notes.R
@@ -1,26 +1,101 @@
-## ----echo=FALSE, message=FALSE, warning = FALSE-----------------------------------------------------------------------
-
+## ----echo=FALSE, message=FALSE, warning = FALSE--------------------------------------------------------
 library(tidyverse)
+library(knitr)
 
-## ---- echo = FALSE----------------------------------------------------------------------------------------------------
+knitr::knit_hooks$set(purl = knitr::hook_purl)
+hook_output = knit_hooks$get('output')
+knit_hooks$set(output = function(x, options) {
+  # this hook is used only when the linewidth option is not NULL
+  if (!is.null(n <- options$linewidth)) {
+    x = xfun::split_lines(x)
+    # any lines wider than n should be wrapped
+    if (any(nchar(x) > n)) x = strwrap(x, width = n)
+    x = paste(x, collapse = '\n')
+  }
+  hook_output(x, options)
+})
 
-install.packages("devtools")
 
-library(devtools)
-devtools::install_github("OI-Biostat/oi_biostat_data")
+## ---- echo = FALSE-------------------------------------------------------------------------------------
 library(oibiostat)
 data("LEAP")
 
-head(LEAP)
+#head(LEAP[,1:4])
+#head(LEAP[,5:6])
 
-## ---- echo=FALSE------------------------------------------------------------------------------------------------------
+kableExtra::kable(LEAP[c(1:5,8),], col.names = c("ID", "Treatment", "Age (in Months)", "Sex", "Ethnicity", "Outcome")) 
+
+
+## ---- echo=FALSE---------------------------------------------------------------------------------------
 table(LEAP$treatment.group, LEAP$overall.V60.outcome)
 
-## ---- echo=FALSE------------------------------------------------------------------------------------------------------
+## ---- echo=FALSE---------------------------------------------------------------------------------------
+library(ggplot2)
+
 x<- as.data.frame(table(LEAP$treatment.group, LEAP$overall.V60.outcome))
 
+
+## ---- echo=FALSE---------------------------------------------------------------------------------------
 x %>% ggplot(aes(Var2, Freq, fill = Var1)) + geom_bar(stat="identity", width=.5, position = "dodge") + xlab("Outcome") + ylab("Count")  + labs(fill="Treatment Group")
 
+
+## ---- echo=FALSE---------------------------------------------------------------------------------------
 x %>% ggplot(aes(Var2, Freq, fill = Var1)) + geom_bar(stat="identity", width=.5, position = "fill") + xlab("Outcome") + ylab("Count")  + labs(fill="Treatment Group")
+
+## ---- echo = FALSE-------------------------------------------------------------------------------------
+
+kableExtra::kable(LEAP[c(1:4),], col.names = c("ID", "Treatment", "Age (in Months)", "Sex", "Ethnicity", "Outcome")) 
+
+
+## ---- results='asis', echo=FALSE, fig.align='center'---------------------------------------------------
+
+i1 <- img_modal(src = "../images/Week2/categorical.png", alt = "Categorical Data")
+
+c(str_split(i1, "\\n", simplify = T)[1:2],
+  str_split(i1, "\\n", simplify = T)[3:9]
+  ) %>% paste(collapse = "\n") %>% cat()
+
+
+## ---- results='asis', echo=FALSE, fig.align='center'---------------------------------------------------
+
+i2 <- img_modal(src = "../images/Week2/numerical.png", alt = "Numerical Data")
+
+c(str_split(i2, "\\n", simplify = T)[1:2],
+  str_split(i2, "\\n", simplify = T)[3:9]
+  ) %>% paste(collapse = "\n") %>% cat()
+
+
+## ---- echo = FALSE-------------------------------------------------------------------------------------
+kableExtra::kable(LEAP[c(1:4, 8),], col.names = c("ID", "Treatment", "Age (in Months)", "Sex", "Ethnicity", "Outcome")) 
+
+
+## ---- echo=FALSE, fig.align='center', out.width="80%"--------------------------------------------------
+
+knitr::include_graphics("../images/Week2/population_sample.png")
+
+
+## ---- echo=FALSE---------------------------------------------------------------------------------------
+
+knitr::include_graphics("../images/Week2/sampling.png")
+
+
+
+## ---- echo=FALSE---------------------------------------------------------------------------------------
+
+knitr::include_graphics("../images/Week2/ice-cream1.png")
+
+
+## ---- echo=FALSE, results='asis'-----------------------------------------------------------------------
+i2 <- img_modal(src = "../images/Week2/ice-cream2.png", alt = "",other=list(width="50%"))
+
+c(str_split(i2, "\\n", simplify = T)[1:2],
+  str_split(i2, "\\n", simplify = T)[3:9]
+  ) %>% paste(collapse = "\n") %>% cat()
+
+
+
+## ---- echo=FALSE---------------------------------------------------------------------------------------
+
+knitr::include_graphics("../images/Week2/random.png")
 
 
