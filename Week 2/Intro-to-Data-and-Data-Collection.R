5ec967e0cff58372971b1a63b612d97d5de6eb09
alisonkleffner <akleffner@huskers.unl.edu>
2024-06-13 17:28:33 -0500

    finish week 2 notes update

---
+++ Week 2/Intro-to-Data-and-Data-Collection.R
@@ -0,0 +1,37 @@
+## Intro-to-Data-and-Data-Collection.pdf R Code
+## Will not go over as a class, just for your reference
+
+
+## ---- echo = FALSE-------------------------------------------------------------------------------------
+
+#To install the 'oibiostat' package (where LEAP data comes from):
+  
+#install.packages("devtools")
+#devtools::install_github("OI-Biostat/oi_biostat_data")
+
+library(oibiostat)
+library(tidyverse)
+
+data("LEAP")
+
+head(LEAP)
+
+## -------------------------------------------------------------------------------------------
+table(LEAP$treatment.group, LEAP$overall.V60.outcome)
+
+## -------------------------------------------------------------------------------------------
+
+x <- as.data.frame(table(LEAP$treatment.group, LEAP$overall.V60.outcome))
+
+## -------------------------------------------------------------------------------------------
+#side by side bar plot
+x %>% ggplot(aes(Var2, Freq, fill = Var1)) + 
+  geom_bar(stat="identity", width=.5, position = "dodge") + 
+  xlab("Outcome") + ylab("Count")  + labs(fill="Treatment Group")
+
+
+## -------------------------------------------------------------------------------------------
+x %>% ggplot(aes(Var2, Freq, fill = Var1)) + 
+  geom_bar(stat="identity", width=.5, position = "fill") + 
+  xlab("Outcome") + ylab("Count")  + labs(fill="Treatment Group")
+
